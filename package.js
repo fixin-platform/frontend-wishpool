b9d507f739137c87df3622a790fa0d8ff39e9b0b
Denis Gorbachev <denis.d.gorbachev@gmail.com>
2015-09-02 11:29:13 +0300

    Move things around

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 var description = {
   summary: "Frontend Wishpool",
   version: "1.0.0",
-  name: "frontend-wishpool"
+  name: "wishpool"
 };
 Package.describe(description);
 
@@ -10,8 +10,8 @@ var fs = Npm.require("fs");
 eval(fs.readFileSync("./packages/autopackage.js").toString());
 Package.onUse(function(api) {
   addFiles(api, description.name, getDefaultProfiles());
-  api.use(["meteor-platform", "coffeescript", "stylus", "mquandalle:jade@0.4.1", "underscore", "jquery"]);
-  api.use(["frontend-foundation@1.0.0"]);
+  //api.use(["meteor-platform", "coffeescript", "stylus", "mquandalle:jade@0.4.1", "underscore", "jquery"]);
+  api.use(["foundation@1.0.0"]);
   api.export([
     "Wishpool",
     "MasterConnection",
